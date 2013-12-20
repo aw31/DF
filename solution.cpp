9f6f54e695c38dc256fc26a45fc4d10bde6b86d6
Alexander Wei <cheddarcheeseyumz@yahoo.com>
2013-12-20 02:11:09 -0500

    Added solve4, modified main.

--- solution.cpp
+++ solution.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <cmath>
+#include <cstdio>
 #include <cstring>
 #include <iomanip>
+#include <sstream>
 #include <vector>
 using namespace std;
 
@@ -88,7 +90,7 @@ grid solve2(grid g){
 
 grid solve3(grid g){
 	grid res;
-	cout << daynum << endl;
+	//cout << daynum << endl;
 	for(int i = 0; i < 30; i++){
 		for(int j = 0; j < 30; j++){
 			if(((j-i+3000)% 3) == ((daynum) % 3)){
@@ -101,8 +103,30 @@ grid solve3(grid g){
 	return res;
 }
 
-const int n_sol = 4;
-solve_func sol[n_sol] = {solve0, solve1, solve2, solve3};
+grid solve4(grid g){
+	// for each cell, increments all neighboring cells by 1/8
+	// with mod 3 observation
+	grid res;
+	for(int i = 0; i<30; i++){
+		for(int j = 0; j<30; j++){
+			if(g(i,j)){
+				for(int k = -1; k<2; k++){
+					for(int l = -1; l<2; l++){
+						if(k!=0||l!=0){
+							if((-daynum+3000)%3==(i+k-j-l+3000)%3) res(i+k, j+l)+=0.3;
+							else if((-daynum+3000)%3==(i+k-j-l+3001)%3) res(i+k, j+l)+=0.1;
+						}
+					}
+				}
+			}
+		}
+	}
+	for(int i = 0; i<30; i++) for(int j = 0; j<30; j++) res(i, j) = min(res(i, j), 1.);
+	return res;
+}
+
+const int n_sol = 5;
+solve_func sol[n_sol] = {solve0, solve1, solve2, solve3, solve4};
 
 grid solve(grid g, int k){
 	return sol[k](g);
@@ -121,17 +145,35 @@ vector<int> find_map(vector<pii> a, vector<pii> b){
 	return res;
 }
 
-int main(){
+vector<double> check(int day){
 
-	// input is day N and day N+1
+	stringstream file;
+	file << "data\\d" << (day<10?"0":"") << day << (day+1<10?"0":"") << day+1 << ".txt";
+	freopen(file.str().c_str(), "r", stdin);
 	string s[30];
 	for(int i = 0; i<30; i++) cin >> s[i];
 	grid start(s);
 	for(int i = 0; i<30; i++) cin >> s[i];
 	grid ans(s);
 	cin >> daynum;
-	vector<int> v = find_map(get_frogs(start), get_frogs(ans));
-	for(int i = 0; i<n_sol; i++) cout << score(ans, solve(start, i)) << endl;
+
+	vector<double> res;
+	for(int i = 0; i<n_sol; i++) res.push_back(score(ans, solve(start, i)));
+	return res;
+
+}
+
+int main(){
+
+	// input is day N and day N+1
+	double avg[n_sol] = {};
+	int D = 15;
+	for(int i = 1; i<=D; i++){
+		vector<double> v = check(i);
+		for(int j = 0; j<n_sol; j++) avg[j]+=v[j]/D;
+	}
+	for(int i = 0; i<n_sol; i++) cout << avg[i] << " ";
+	cout << endl;
 
 }
 
