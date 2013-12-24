c5231d478504dfe25c0603b8b8d6a83ba09dcea1
ksun48@gmail.com <kevin_work@toshi3.(none)>
2013-12-23 22:35:18 -0600

    added gridlines in draw?

--- draw.cpp
+++ draw.cpp
@@ -8,11 +8,30 @@
 using namespace std;
 
 int draw(grid g, string name){
+        char arr[900*64];
+        for(int i = 0; i<30; i++){
+                for(int j = 0; j<30; j++){
+                        for(int k = 0; k<8; k++){
+                                for(int l = 0; l<8; l++) arr[30*(64*i+8*k)+8*j+l] = (1-g(i,j))*255;
+                        }
+                }
+        }
+        return stbi_write_png(name.c_str(), 240, 240, 1, arr, 240);
+}
+int draw2(grid g, string name){
 	char arr[900*64];
 	for(int i = 0; i<30; i++){
 		for(int j = 0; j<30; j++){
 			for(int k = 0; k<8; k++){
-				for(int l = 0; l<8; l++) arr[30*(64*i+8*k)+8*j+l] = (1-g(i,j))*255;
+				for(int l = 0; l<8; l++){
+					if(k % 8 && l % 8 ){
+						arr[30*(64*i+8*k)+8*j+l] = (1-g(i,j))*253+1;
+					} else if(!((!(k%8)&& i%5) || (!(l%8)&& j%5))){
+						arr[30*(64*i+8*k)+8*j+l] = 0;
+					} else {
+						arr[30*(64*i+8*k)+8*j+l] = 127;
+					}
+				}
 			}
 		}
 	}
