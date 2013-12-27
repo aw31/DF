d315aa84f2060728bb26df5793f98771afe7a95d
Alexander Wei <cheddarcheeseyumz@yahoo.com>
2013-12-27 14:15:11 -0500

    Cleaned things up.

--- test.cpp
+++ test.cpp
@@ -13,34 +13,10 @@ const int D = 23;
 int daynum;
 
 grid start[40], ans[40];
-int freq[131072], tot[131072];
 
 grid solve(grid g, double a1, double a2, double a3, double a4){
 
 	grid res;
-	for(int i = 0; i<30; i++){
-		for(int j = 0; j<30; j++){
-			double n_adj = 0;
-			for(int k = -1; k<2; k++) for(int l = -1; l<2; l++) if(k||l) n_adj+=g(i+k,j+l);
-			if((-daynum+3000)%3==(i-j+3000)%3){
-				if(n_adj==2) res(i, j)+=0.9;
-				else if(n_adj>=3) res(i,j)+=0.95;
-				if(g(i, j)) res(i, j)-=0.4;
-			} else if((-daynum+3000)%3==(i-j+3001)%3){
-				if(n_adj==2) res(i,j)+=0.2;
-				else if(n_adj>=3) res(i,j)+=0.3;
-				if(g(i, j)) res(i, j)-=0.15;
-			}
-			int st = 0;
-			for(int k = -2; k<2; k++) for(int l = -2; l<2; l++) st = 2*st+g(i+k,j+l); 
-			if((-daynum+3000)%3==(i-j+3000)%3) st = 2*st+1;
-			else if((-daynum+3000)%3==(i-j+3001)%3) st*=2;
-			else continue;
-			if(tot[st]>a2){
-				res(i,j) = 1.0*freq[st]/tot[st];
-			}
-		}
-	}
 	res.fix();
 	return res;
 
@@ -81,8 +57,6 @@ int main(){
 
 	for(int i = 1; i<=D; i++) load(i);
 	double best = 0, bi, bj, bk, bl;
-	ifstream fin("solve7-1.txt");
-	for(int i = 0; i<131072; i++) fin >> freq[i] >> tot[i];
 	for(double i = 0; i<=0.2; i+=0.01){
 		cout << i <<endl;
 		for(int j = 1; j<300; j++){
